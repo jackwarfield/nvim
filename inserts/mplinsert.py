c3a268febb6b3cdeca2ccf68bb9192e1fb63ce5f
Jack Warfield <jtw5zc@virginia.edu>
2023-01-20 12:33:49 -0500

    pieces of text for insert commands

---
+++ inserts/mplinsert.py
@@ -0,0 +1,8 @@
+plt.rc('xtick', top=True, direction='in', labelsize=15)
+plt.rc('ytick', right=True, direction='in', labelsize=15)
+plt.rc('font', family='Arial')
+
+scarlet = '#bb0000'
+grey = '#666666'
+rotorange = '#E57200'
+jeffblue = '#232D4B'
